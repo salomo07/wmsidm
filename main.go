0d43cf2ed26eb9999aaa6ed8624e20ced5060b6b
Raja Salomo Sitompul <raja.sitompul@adira.co.id>
2023-03-08 17:02:31 +0700

    Coba bikin gin-swagger

--- main.go
+++ main.go
@@ -5,14 +5,41 @@ import (
 	"wmsidm/routers"
 
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
+	// @title           Kukuruyuk.com
+	// @version         1.0
+	// @description     Ini description, tolong dibaca dulu.
+	// @termsOfService  http://swagger.io/terms/
+
+	// @contact.name   API Support
+	// @contact.url    http://www.swagger.io/support
+	// @contact.email  support@swagger.io
+
+	// @license.name  Apache 2.0
+	// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+	// @host      localhost:8080
+	// @BasePath  /api/v1
+
+	// @securityDefinitions.basic  BasicAuth
+
+	// @externalDocs.description  OpenAPI
+	// @externalDocs.url          https://swagger.io/resources/open-api/
+
+	port := "7890"
+	// os.Getenv("PORT")
 	r := gin.Default()
+
 	routers.IDMRouter(r)
-	r.NoRoute(func(c *gin.Context) {
-		c.JSON(404, gin.H{"code": "404", "message": "Page not found"})
-	})
-	// port := os.Getenv("PORT")
-	r.Run(":7890")
+	// r.NoRoute(func(c *gin.Context) {
+	// 	c.JSON(404, gin.H{"code": "404", "message": "Endpoint is not found"})
+	// })
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	// r.Run()
+	r.Run(":" + port)
 }
--- routers/auth_r.go
+++ routers/auth_r.go
@@ -1,9 +1,8 @@
 package routers
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
+	docs "wmsidm/docs"
 )
 
 type RequestLog struct {
@@ -15,25 +14,25 @@ type RequestLog struct {
 }
 
 func IDMRouter(r *gin.Engine) {
-	// r.Any("", func(c *gin.Context) {
-	// 	log.Println("Ini dari : ", c.Request.Host+c.Request.URL.String())
-	// })
-	// r.Any("/idm/api/v1/authenticate", func(c *gin.Context) {
-	// 	c.JSON(200, map[string]interface{}{"Lalala": "Yeyeye"})
-	// 	log.Println("Ini dari : ", c.Request.Host+c.Request.URL.String())
-	// })
-	// r.Any("/*path", func(c *gin.Context) {
-	// 	log.Println("dfgx")
-	// })
-	// r.GET("/idm/api/v1/authenticate", func(c *gin.Context) {
-	// 	log.Println("df", c.Request.Host+c.Request.URL.String())
-	// })
+	docs.SwaggerInfo.BasePath = "/api/v1"
 	idmv1 := r.Group("/idm/api/v1/")
 	{
+		// ShowAccount godoc
+		// @Summary      Show an account
+		// @Description  get string by ID
+		// @Tags         accounts
+		// @Accept       json
+		// @Produce      json
+		// @Param        id   path      int  true  "Account ID"
+		// @Success      200  {object}  model.Account
+		// @Failure      400  {object}  httputil.HTTPError
+		// @Failure      404  {object}  httputil.HTTPError
+		// @Failure      500  {object}  httputil.HTTPError
+		// @Router       /accounts/{id} [get]
 		idmv1.GET("authenticate?:name", func(c *gin.Context) {
-			log.Println("df", c.Query("name"), c.Request.Host+c.Request.URL.String())
-			c.JSON(200, map[string]interface{}{"Lalala": "Yeyeye"})
-			log.Println("Ini dari : ", c.Request.Host+c.Request.URL.String())
+			// log.Println("df", c.Query("name"), c.Request.Host+c.Request.URL.String())
+			c.JSON(200, map[string]interface{}{"Lalala": "Yeyeye", "datauser": c.Query("qw")})
+			// log.Println("Ini dari : ", c.Request.Host+c.Request.URL.String())
 		})
 	}
 }
